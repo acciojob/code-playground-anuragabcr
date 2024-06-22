bfa74c68ae535313ce735f3f3237b4aac624cff9
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-22 10:33:55 +0000

    Code Playground

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,85 @@
-
 import React from "react";
-import './../styles/App.css';
+import {
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+import Login from "./Login";
+import Home from "./Home";
+import NotFound from "./NotFound";
+
+// Custom PrivateRoute component
+const PrivateRoute = ({
+  component: Component,
+  isAuthenticated,
+  redirect,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={redirect} />
+      )
+    }
+  />
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    this.setState({ isLoggedIn: !this.state.isLoggedIn });
+  }
+
+  render() {
+    const { isLoggedIn } = this.state;
+    return (
+      <div className="main-container">
+        <div>
+          {isLoggedIn
+            ? "Logged in, Now you can enter Playground"
+            : "You are not authenticated, Please login first"}
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link to="/home">PlayGround</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route
+            path="/login"
+            render={(props) => (
+              <Login {...props} login={this.login} isLogged={isLoggedIn} />
+            )}
+          />
+          <PrivateRoute
+            exact
+            path="/home"
+            component={Home}
+            isAuthenticated={isLoggedIn}
+            redirect="/login"
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
